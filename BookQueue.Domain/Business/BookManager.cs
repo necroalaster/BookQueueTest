a5860d501c233ecf1df103cd26eb9c5e00e51fc3
necroalaster <45764308+necroalaster@users.noreply.github.com>
2023-07-24 18:41:27 -0300

    Added the class BookManager which will interact with the calls from the endpoints and call repositories according to the situation.
    Added conversion of types with automapper
    
    Added tests for the BookManager class.

---
+++ BookQueue.Domain/Business/BookManager.cs
@@ -0,0 +1,75 @@
+﻿using AutoMapper;
+using BookQueue.Domain.Entities;
+using BookQueue.Domain.Interfaces;
+using BookQueue.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookQueue.Domain.Business
+{
+    public class BookManager : IBookManager
+    {
+        private readonly IAuthorRepository _authorRepository;
+        private readonly IBookRepository _bookRepository;
+        private readonly IMapper _mapper;
+        
+        public BookManager(IAuthorRepository authorRepository, IBookRepository bookRepository, IMapper mapper)
+        {
+            _authorRepository = authorRepository;
+            _bookRepository = bookRepository;
+            _mapper = mapper;
+        }
+
+        public IEnumerable<BookModel> GetAllBooks()
+        {
+            var bookList = _bookRepository.GetAll();
+
+            return _mapper.Map<List<Book>, List<BookModel>>(bookList.ToList());
+        }
+
+        public BookModel GetBookById(Guid id)
+        {
+            var book = _bookRepository.GetByID(id);
+
+            return _mapper.Map<BookModel>(book);
+        }
+
+        public void DeleteBook(Guid id)
+        {
+            _bookRepository.Delete(id);
+        }
+
+        public void SaveBook(BookModel bookModel)
+        {
+            var book = _mapper.Map<Book>(bookModel);
+
+            var author = _authorRepository.GetByname(bookModel.AuthorName);
+
+            if (author == null)
+            {
+                author = CreateAuthor(bookModel.AuthorName);
+            }
+
+            book.Author = author;
+            book.AuthorId = author.Id;
+
+            _bookRepository.Save(book);
+        }
+
+        private Author CreateAuthor(string authorName)
+        {
+            _authorRepository.Save(
+                new Author()
+                {
+                    CreatedAt = DateTime.Now,
+                    ModifiedAt = DateTime.Now,
+                    Name = authorName
+                }
+            );
+
+            return _authorRepository.GetByname(authorName);
+        }
+    }
+}

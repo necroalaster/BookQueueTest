eec01bafe9b8e11ff7c462d80f051494d581161a
necroalaster <45764308+necroalaster@users.noreply.github.com>
2023-07-24 15:05:11 -0300

    Added repository classes Added interface for repositories Added unit tests for repositories

---
+++ BookQueue.Domain/Interfaces/IAuthorRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookQueue.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookQueue.Domain.Interfaces
+{
+    public interface IAuthorRepository
+    {
+        void Save(Author author);
+
+        Author GetByname(string name);
+    }
+}
---
+++ BookQueue.Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,60 @@
+﻿using BookQueue.Domain.Entities;
+using BookQueue.Domain.Interfaces;
+using BookQueue.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookQueue.Infrastructure.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private BookQueueContext _context;
+
+        public BookRepository(BookQueueContext context)
+        {
+            _context = context;
+        }
+
+        public void Delete(Guid bookId)
+        {
+            var book = _context.Books.Where(x => x.Id == bookId).SingleOrDefault();
+
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+                _context.SaveChanges();
+            }
+                
+        }
+
+        public IEnumerable<Book> GetAll()
+        {
+            return _context.Books
+                .OrderBy(x => x.Title)
+                .AsNoTracking();
+        }
+
+        public Book GetByID(Guid bookId)
+        {
+            return _context.Books.Where(x => x.Id == bookId).SingleOrDefault();
+        }
+
+        public void Save(Book book)
+        {
+            var oldBook = _context.Books.Where(x => x.Id == book.Id).SingleOrDefault();
+
+            if (oldBook != null)
+            {
+                oldBook.PublicationYear = book.PublicationYear;
+                oldBook.Title = book.Title;
+            }
+            else
+                _context.Books.Add(book);
+
+            _context.SaveChanges();
+        }
+    }
+}
